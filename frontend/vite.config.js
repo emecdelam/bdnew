2c0cff3eea13164d70457dcdcc21bba4d358ecfa
Théo Daron <theo@daron.be>
2026-02-20 14:34:27 +0100

    feat: prod and dev compose

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['.kapucl.be', 'kotbd.kapucl.be'],
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8001',
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const backendUrl = env.VITE_BACKEND_URL || 'http://127.0.0.1:8001'
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      allowedHosts: ['.kapucl.be', 'kotbd.kapucl.be'],
+      proxy: {
+        '/api': {
+          target: backendUrl,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-})
+  })
+}
